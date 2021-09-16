629ca782455139eef29e5b3941246841b7ab9cec
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-09-16 12:18:28 +0200

    defining wilder service operations

--- services/wilderService.js
+++ services/wilderService.js
@@ -1,1 +1,19 @@
 const wilderDAO = require('../dao/wilderDAO');
+
+module.exports = {
+    read: (id) => {
+        //TODO impl
+    },
+    readAll: () => {
+        //TODO impl
+    },
+    create: (name, city, skills) => {
+        //TODO impl
+    },
+    update: (name, city, skills) => {
+        //TODO impl
+    },
+    delete: (id) => {
+        //TODO impl
+    },
+}
