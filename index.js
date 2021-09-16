37af4f190e0212f0d55493455c0eefa47841dfb1
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-09-16 11:12:50 +0200

    init mongodb connection

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+//init mongodb connection
+const mongoose = require('mongoose');
+// connection string: mongodb protocol + db server host + db name
+mongoose
+    .connect('mongodb://127.0.0.1:27017/wilderdb')
+    .then(() => console.log('Connected to wildersdb database'))
+    .catch((err) => console.log(`Error while connecting to database: ${err.message}`));
+
+app.listen(8080, (err) => {
+    if(err) {
+        console.error('Error while launching the server port: 8080');
+    } else {
+        console.log('Server started on port 8080')
+    }
+});
