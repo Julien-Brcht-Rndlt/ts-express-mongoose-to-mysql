f203c09d524f9ef0f322698be04e3a75c3fda6aa
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-09-25 18:11:49 +0200

    declaring mysql db config into dedicated file

--- src/db-config.ts
+++ src/db-config.ts
@@ -1,15 +1,15 @@
-import mysql, { Connection } from 'mysql2';
+import mysql, { ConnectionOptions } from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connection: Connection = mysql.createConnection({
+const dbConfig: ConnectionOptions = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT as unknown as number,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     multipleStatements: true,
-});
+};
 
-export default connection;
+export default dbConfig;
