478726d24153d4d567adf6418ff703bdbeb72669
Julien Nicolas <79537192+Julien-Brcht-Rndlt@users.noreply.github.com>
2021-09-25 20:03:59 +0200

    Delete dist directory

--- dist/middlewares/errorsHandlers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleError = void 0;
-const handleError = (err, req, res, next) => {
-    res.status(500).json({
-        message: err.message
-    });
-};
-exports.handleError = handleError;
--- dist/models/WilderModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const WilderSchema = new Schema({
-    wilderName: { type: String, unique: true },
-    city: String,
-    skills: [{ title: String, votes: Number }],
-});
-module.exports = mongoose.model('wilder', WilderSchema);
