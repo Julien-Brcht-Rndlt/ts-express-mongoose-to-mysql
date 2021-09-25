b47386e3feffe6df01024393bd85fee856b1bef6
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-09-25 18:08:44 +0200

    adapting models to DAL based on MySQL db

--- src/models/Skill.ts
+++ src/models/Skill.ts
@@ -1,6 +1,12 @@
-interface Skill {
-    title: string;
-    votes: number;
+class Skill {
+    id: number;
+    title: string = '';
+    votes: number = 0;
+
+    constructor (values: [number, string, number]) {
+        this.id = values[0];
+        this.title = values[1];
+    }
 };
 
 export default Skill;
--- src/models/Wilder.ts
+++ src/models/Wilder.ts
@@ -1,7 +1,14 @@
 import Skill from './Skill';
 
-export default interface Wilder {
-    wilderName: string;
-    city: string;
-    skills: Array<Skill>;
+export default class Wilder {
+    id: number;
+    wilderName: string = '';
+    city: string = '';
+    skills: Array<Skill> = [];
+
+    constructor(values: any[]) {
+        this.id = typeof values[0] === 'string' ? parseInt(values[0]) : values[0];
+        this.wilderName = values[1];
+        this.city = values [2];
+    }
 };
